28bcc4e80a7fbd09c1704b54d583440fcef712d3
vidya kharje <vidya.kharje@sita.aero>
2024-04-26 12:35:12 +0100

    feat: add iam role

---
+++ examples/ec2-infra/iam.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_policy" "session_manager_policy" {
+  name        = "SSMSessionManagerPolicy"
+  description = "Allows access for Session Manager to manage EC2 instances"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:StartSession",
+          "ssm:ResumeSession",
+          "ssm:TerminateSession",
+          "ssm:DescribeSessions",
+          "ssm:GetSessionDocument"
+        ],
+        "Resource" : ["*"]
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name                = "SSMSessionManagerRole"
+  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
+  managed_policy_arns = [aws_iam_policy.session_manager_policy.arn]
+}
